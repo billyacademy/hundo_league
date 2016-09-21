7ac190e854603e665dba1730f3d8503d778a784c
William Mahoney <wmahoney@purch.com>
2016-09-21 09:08:23 -0400

    added previous week's rank & some more styling

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -21,9 +21,12 @@ class WeeksController < ApplicationController
     @week = Week.where('week_number' => '0')
     @rankings = Ranking.all.collect { |ranking|  {id: ranking.id, team_id: ranking.team_id, week_id: ranking.week_id, rank: ranking.rank, comment: ranking.comment}}
     @week_rankings = []
+    @previous_week_rankings = []
     @rankings.each do |ranking|
       if ranking[:week_id] == params[:id].to_i
         @week_rankings.push(ranking)
+      elsif ranking[:week_id] == params[:id].to_i - 1
+        @previous_week_rankings.push(ranking)
       end
     end
     @teams = Team.all.collect { |team| {id: team.id, name: team.name, owner: team.owner}}
